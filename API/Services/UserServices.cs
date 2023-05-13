7880e91368a206493b3239fd6e5e338b31f7b658
stasex <stasek.molchan@gmail.com>
2023-05-13 16:24:56 +0300

    add new services for user

---
+++ API/Services/UserServices.cs
@@ -0,0 +1,22 @@
+﻿using Domain.Models;
+using Microsoft.EntityFrameworkCore;
+using Persistence;
+using API.Services.Interfaces;
+
+
+namespace API.Services;
+
+public class UserServices:IUserService
+{
+    private readonly DataContext _context;
+    
+    public UserServices(DataContext context)
+    {
+        _context = context;
+    }
+    
+    public async Task<List<User>> GetAllUsersAsync()
+    {
+        return await _context.Users.ToListAsync();
+    }
+}
