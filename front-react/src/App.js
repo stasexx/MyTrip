bf7bad51b997fed4d02c59e54ef4e817f855ad68
AsTroLog <tarasbondarenko3@gmail.com>
2023-05-19 12:12:09 +0300

    add folder for pages

--- front-react/src/App.js
+++ front-react/src/App.js
@@ -1,16 +1,25 @@
 import "./styles/main.css"; 
 import NavBar from "./Components/navbar/NavBar"
-import MainPge from "./pages/MainPage"
+import MainPge from "./pages/MainPage/MainPage"
+import Cabinet from "./pages/Cabinet/Cabinet"
 import Footer from "./Components/footer/Footer"
-
+import {BrowserRouter as Router,Routes,Route} from "react-router-dom"
 function App() {
 
   return (
     
     <div className="App">
+      <Router>
+
         <NavBar/>
-        <MainPge/>
+          <Routes>
+              <Route path="/" element={<MainPge/>}/>
+              <Route path="/cabinet" element={<Cabinet/>}/>
+          </Routes>
+
         <Footer/>
+
+      </Router>
     </div>
   );
 }
