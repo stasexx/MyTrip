bf145fdc7ef500fbae6175dbcf6400ec87702728
AsTroLog <tarasbondarenko3@gmail.com>
2023-05-19 10:21:45 +0300

    create mainpage

--- front-react/src/Components/tour/Tour.js
+++ front-react/src/Components/tour/Tour.js
@@ -1,16 +1,41 @@
 import"./style.css";
+import StRating from './rating';
+import wish_button from "./../../img/icon/wish_button.png";
+import wish_button_black from "./../../img/icon/wish_button_black.png";
 
-const Tour = () => {
+
+
+const Tour = ({name, main_photo, rate}) => {
+
+
+    function show(e) {
+
+        var btn = document.getElementById(2);
+
+        btn.addEventListener("click", function() {
+            if(this.classList.contains('wish')){
+                this.classList.remove('wish');
+            }else {
+                this.classList.add('wish');
+            }
+        });
+    }
+
     return ( 
     <>
-    <li className="tour">
-        <a href="./project-page.html">
-            <img src="./img/projects/01.jpg" alt="Project img" className="tour__img"/>
-            <h3 className="tour__title">Gaming streaming portal</h3>
-        </a>
-    </li>
-    
-    </> );
+        <li id="dfg"className="tour">
+            <a href="./project-page.html">
+                <img src={main_photo} alt="Project img" className="tour__img"/>
+                <h3 className="tour__title">{name}</h3>
+                <div className="tour__rating"><StRating rate={rate}/></div>
+            </a>
+            <a id="2" className="wishlist" onClick={show} >
+                <img src={wish_button} alt="" className="like__img"/>
+            </a>
+        </li>
+
+    </> 
+    );
 }
  
 export default Tour;
