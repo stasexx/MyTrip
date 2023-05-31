3c904cc90ecf89c69cf190b332dfe098854e00f7
AsTroLog <tarasbondarenko3@gmail.com>
2023-05-31 13:54:15 +0300

    tour page

--- front-react/src/Components/navbar/NavBar.js
+++ front-react/src/Components/navbar/NavBar.js
@@ -2,6 +2,10 @@ import"./styles.css"
 
 import logo from "./../../img/icon/logo.png"; 
 
+
+import React, {useEffect, useState} from "react";
+import axios from 'axios';
+
 import wish_list from "./../../img/icon/wish_list.png"; 
 import tours from "./../../img/icon/tours.png"; 
 import sign_in from "./../../img/icon/sign_in.png"; 
@@ -9,12 +13,27 @@ import about_us from "./../../img/icon/about_us.png";
 import search_glass from "./../../img/icon/glass_search.png";
 
 const  NavBar= () => {
+
+
+    const[user,setUser] = useState([]);
+    var user_id = localStorage.getItem("login");
+
+        useEffect(()=>{
+            if(user_id){
+                axios.get(`http://localhost:5000/api/Users/get/userById=${user_id}`)
+                .then(data =>{
+                    setUser(data.data);
+                })
+            }
+        
+        },[]);
+
     return ( 
     <>
     <nav className="nav">
         <div className="container">
             <div className="nav-row">
-                <a href="./" className="logo"><img src={logo} alt="Link"/></a>
+                <a href="/" className="logo"><img src={logo} alt="Link"/></a>
                 <div class ="search-box">
                      <a class="search-btn" href="#"><img className="icon_header" src={search_glass} alt="Link"/></a>
                     <input class="search-tour" type ="text" name="" placeholder="Search tours"></input>
@@ -23,7 +42,19 @@ const  NavBar= () => {
                         <li className="nav-list__item"><a href="./index.html" className="nav-list__link"><img className="icon_header" src={about_us} alt="Link"/>About us</a></li>
                         <li className="nav-list__item"><a href="./projects.html" className="nav-list__link"><img className="icon_header" src={tours} alt="Link"/>Tours</a></li>
                         <li className="nav-list__item"><a href="./contacts.html" className="nav-list__link"><img className="icon_header" src={wish_list}  alt="Link"/>Wish list</a></li>
-                        <li className="nav-list__item"><a href="./login" className="nav-list_sign"><img className="icon_header" src={sign_in} alt="Link"/>Sign in</a></li>
+                    
+                    
+                        {user_id ? (
+
+                               <li className="nav-list__item"><a href="./cabinet" className="nav-list_user"><img className="icon_user" src={user.avatar} alt="Link" /></a></li>
+
+                          ) : (
+                            <li className="nav-list__item"><a href="./login" className="nav-list_sign"><img className="icon_header" src={sign_in} alt="Link" />Sign in</a></li>
+                          )}
+                    
+                    
+                    
+                    
                     </ul>
             </div>
         </div>
