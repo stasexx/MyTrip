9cae1f7e367825fe67d38e86a161f90aaded6709
stasex <stasek.molchan@gmail.com>
2023-06-03 22:44:03 +0300

    add new logic for admin

---
+++ API/Controllers/AdminController.cs
@@ -0,0 +1,44 @@
+﻿using API.Services.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers;
+
+public class AdminController:BaseApiController
+{
+    private readonly IAdminService _adminService;
+
+    public AdminController(IAdminService adminService)
+    {
+        _adminService = adminService;
+    }
+
+    [HttpPost("ban/banUser/userId={userId}")]
+    public async Task<bool> BanUser(int userId)
+    {
+        return await _adminService.BanUser(userId);
+    }
+    
+    [HttpPost("ban/unbanUser/userId={userId}")]
+    public async Task<bool> UnBanUser(int userId)
+    {
+        return await _adminService.UnBanUser(userId);
+    }
+    
+    [HttpPost("delete/deleteUser/userId={userId}")]
+    public async Task<bool> DeleteUser(int userId)
+    {
+        return await _adminService.DeleteUser(userId);
+    }
+    
+    [HttpGet("stats/countOfAllUsers")]
+    public async Task<int> CountOfAllUsers()
+    {
+        return await _adminService.GetCountOfAllUsers();
+    }
+    
+    [HttpGet("stats/countOfAllTours")]
+    public async Task<int> CountOfAllTours()
+    {
+        return await _adminService.GetCountOfAllTours();
+    }
+}
---
+++ API/Services/Interfaces/IAdminService.cs
@@ -0,0 +1,14 @@
+﻿namespace API.Services.Interfaces;
+
+public interface IAdminService
+{
+    Task<bool> BanUser(int userId);
+
+    Task<bool> UnBanUser(int userId);
+
+    Task<bool> DeleteUser(int userId);
+
+    Task<int> GetCountOfAllUsers();
+    
+    Task<int> GetCountOfAllTours();
+}
