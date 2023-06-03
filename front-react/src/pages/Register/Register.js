9719dbd43aca22b3e5316d55a4c4fb668c2871a9
AsTroLog <tarasbondarenko3@gmail.com>
2023-06-03 23:51:39 +0300

    87

--- front-react/src/pages/Register/Register.js
+++ front-react/src/pages/Register/Register.js
@@ -4,7 +4,7 @@ import axios from 'axios';
 import login_photo_right from "./../../img/icon/login_photo_right.png";
 import logo from "./../../img/icon/logo.png"; 
 import google from "./../../img/icon/google.png"; 
-import {useNavigate} from 'react-router-dom';
+import {useNavigate,useLocation} from 'react-router-dom';
 const src_google="http://localhost:5000/api/GoogleOAuth/oauth/authorization";
 
 
@@ -35,17 +35,80 @@ const  Login = () => {
             alert("Ви не заповнили всі поля")
         }
     }
-      function Google_login(){
+    const location = useLocation();
+    var YOUR_CLIENT_ID = '181736966870-p9ttm86o2f4586i07ldte8jjajc7e2m4.apps.googleusercontent.com';
+    var YOUR_REDIRECT_URI = 'http://localhost:3000/login';
+    var fragmentString = location.hash.substring(1);
 
-        try {
-             axios.get(src_google)
-            .then(res => console.log(res.data))
-
-          } catch (error) {
-            console.log('Помилка запиту:', error);
-          }
-
+    // Parse query string to see if page request is coming from OAuth 2.0 server.
+    var params = {};
+    var regex = /([^&=]+)=([^&]*)/g, m;
+    while (m = regex.exec(fragmentString)) {
+    params[decodeURIComponent(m[1])] = decodeURIComponent(m[2]);
     }
+    if (Object.keys(params).length > 0) {
+    localStorage.setItem('oauth2-test-params', JSON.stringify(params) );
+    if (params['state'] && params['state'] == 'try_sample_request') {
+        trySampleRequest();
+    }
+    }
+
+    // If there's an access token, try an API request.
+    // Otherwise, start OAuth 2.0 flow.
+    function trySampleRequest() {
+        var params = JSON.parse(localStorage.getItem('oauth2-test-params'));
+        if (params && params['access_token']) {
+          var xhr = new XMLHttpRequest();
+          xhr.open('GET', 'https://www.googleapis.com/oauth2/v1/userinfo?access_token=' + params['access_token']);
+          xhr.onreadystatechange = function (e) {
+            if (xhr.readyState === 4 && xhr.status === 200) {
+
+              console.log(xhr.responseText);
+
+            } else if (xhr.readyState === 4 && xhr.status === 401) {
+              // Token invalid, so prompt for user permission.
+              oauth2SignIn();
+            }
+          };
+          xhr.send(null);
+        } else {
+          oauth2SignIn();
+        }
+      }
+
+    /*
+    * Create form to request access token from Google's OAuth 2.0 server.
+    */
+    function oauth2SignIn() {
+    // Google's OAuth 2.0 endpoint for requesting an access token
+    var oauth2Endpoint = 'https://accounts.google.com/o/oauth2/v2/auth';
+
+    // Create element to open OAuth 2.0 endpoint in new window.
+    var form = document.createElement('form');
+    form.setAttribute('method', 'GET'); // Send as a GET request.
+    form.setAttribute('action', oauth2Endpoint);
+
+    // Parameters to pass to OAuth 2.0 endpoint.
+    var params = {'client_id': YOUR_CLIENT_ID,
+                    'redirect_uri': YOUR_REDIRECT_URI,
+                    'scope': 'https://www.googleapis.com/auth/drive.metadata.readonly',
+                    'state': 'try_sample_request',
+                    'include_granted_scopes': 'true',
+                    'response_type': 'token'};
+
+    // Add form parameters as hidden input values.
+    for (var p in params) {
+        var input = document.createElement('input');
+        input.setAttribute('type', 'hidden');
+        input.setAttribute('name', p);
+        input.setAttribute('value', params[p]);
+        form.appendChild(input);
+    }
+
+    // Add form to page and submit it to open the OAuth 2.0 endpoint.
+    document.body.appendChild(form);
+    form.submit();
+}
     
         return ( 
             
@@ -68,7 +131,7 @@ const  Login = () => {
                                 <div className="line1"></div><div className="text_or">or</div><div className="line2"></div>
                             </div>
                             <div className="login_button">
-                                <button onClick={Google_login} className="google_acc"><img className="foto_google" src={google} alt="Link" />Continue with Google</button>
+                                <button onClick={() => oauth2SignIn()} className="google_acc"><img className="foto_google" src={google} alt="Link" />Continue with Google</button>
                                 <button href="./" onClick={Check_register} className="log">Register</button>
                                 <div className="quest">Already have an account?<a href="./login" className="signup">Sign in</a></div>
                             </div>
