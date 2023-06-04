68260f31905151aa287e3cf559727bd88a1d2737
stasex <stasek.molchan@gmail.com>
2023-06-04 15:30:17 +0300

    add query for tour photos service

--- API/Controllers/TourPhotosController.cs
+++ API/Controllers/TourPhotosController.cs
@@ -19,4 +19,23 @@ public class TourPhotosController:BaseApiController
     {
         return await _tourPhotos.GetTourPhotosByTourId(id);
     }
+    
+    
+    [HttpPost("create/tourPhoto/tourId={tourId}/uuid={uuid}")]
+    public async Task<bool> AddTourPhotosByTourId(int tourId, string uuid)
+    {
+        return await _tourPhotos.AddTourPhotosByTourId(tourId, uuid);
+    }
+
+    [HttpPost("delete/deleteTourPhotoById/id={id}")]
+    public async Task<bool> RemoveTourPhotoById(int id)
+    {
+        return await _tourPhotos.RemoveTourPhotoById(id);
+    }
+
+    [HttpPost("delete/deleteAllTourPhotoByTourId/tourId={tourId}")]
+    public async Task<bool> RemoveAllTourPhotosByTourId(int tourId)
+    {
+        return await _tourPhotos.RemoveAllTourPhotosByTourId(tourId);
+    }
 }
--- API/Services/TourPhotosService.cs
+++ API/Services/TourPhotosService.cs
@@ -18,4 +18,41 @@ public class TourPhotosService:ITourPhotosService
     {
         return await _context.TourPhotos.Where(pt => pt.Tour.TourId == id).ToListAsync();
     }
+
+    public async Task<bool> AddTourPhotosByTourId(int tourId, string uuid)
+    {
+        var tour = await _context.Tours.FirstOrDefaultAsync(t => t.TourId == tourId);
+        if (tour!=null)
+        {
+            var tourPhoto = new TourPhoto()
+            {
+                Tour = tour,
+                UUID = uuid
+            };
+            await _context.TourPhotos.AddAsync(tourPhoto);
+            await _context.SaveChangesAsync();
+            return true;
+        }
+
+        return false;
+    }
+
+    public async Task<bool> RemoveTourPhotoById(int id)
+    {
+        var tourPhoto = await _context.TourPhotos.FirstOrDefaultAsync(p => p.TourPhotoId == id);
+        if (tourPhoto!=null)
+        {
+            _context.TourPhotos.Remove(tourPhoto);
+            await _context.SaveChangesAsync();
+            return true;
+        }
+
+        return false;
+    }
+    
+    public async Task<bool> RemoveAllTourPhotosByTourId(int tourId)
+    {
+        await _context.TourPhotos.Where(t=>t.Tour.TourId==tourId).ExecuteDeleteAsync();
+        return true;
+    }
 }
