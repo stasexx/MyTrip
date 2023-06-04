97bd99869e4cc4c3d45d1f44aa8b23b221fd1c28
stasex <stasek.molchan@gmail.com>
2023-06-04 14:59:17 +0300

    new query for orders

--- API/Controllers/OrderController.cs
+++ API/Controllers/OrderController.cs
@@ -18,4 +18,16 @@ public class OrderController:BaseApiController
     {
         return await _orderService.CreateOrder(orgTourId, userId);
     }
+    
+    [HttpPost("delete/deleteOrder/orderId={orderId}")]
+    public async Task<bool> DeleteOrder(int orderId)
+    {
+        return await _orderService.DeleteOrder(orderId);
+    }
+    
+    [HttpPost("change/changeDateOfOrder/orderId={orderId}/newDate={newDate}")]
+    public async Task<bool> ChangeDateOfOrder(int orderId, DateTime newDate)
+    {
+        return await _orderService.ChangeDate(orderId, newDate);
+    }
 }
